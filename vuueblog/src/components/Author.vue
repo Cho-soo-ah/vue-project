12f582274cbdc0c9003a65bd06a72945dc2637da
Cho-soo-ah <83620662+Cho-soo-ah@users.noreply.github.com>
2022-06-27 15:52:22 +0900

    Delete vuueblog/src/components directory

--- vuueblog/src/components/Author.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <h4>작가소개</h4>
-</template>
-
-<script>
-export default { name: "blogAuthor" };
-</script>
-
-<style></style>
--- vuueblog/src/components/Comment.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <h4>댓글</h4>
-</template>
-
-<script>
-export default { name: "blogAuthor" };
-</script>
-
-<style></style>
